5d2a9d70aa701b6fdf2b8f572060afab865a08d9
Sonya Schloss <soschloss@ucsd.edu>
2026-05-07 20:27:53 -0700

    Add meta page, loc.csv, and npm config files

--- global.js
+++ global.js
@@ -10,6 +10,7 @@ let pages = [
   { url: 'projects/', title: 'Projects' },
   { url: 'resume/', title: 'Resume' },
   { url: 'contact/', title: 'Contact' },
+  { url: 'meta/', title: 'Meta' },
   { url: 'https://github.com/soschloss', title: 'GitHub' },
 ];
 let nav = document.createElement('nav');
